be7b570c6f9055c319e6a50e63e6c5774892ccfe
Manthan <kafax55297@abaot.com>
2025-04-05 20:35:02 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -11,4 +11,5 @@ echo "==========================Simpleangularapp container has been removed=====
 echo "admin" | sudo -S docker run -d --name simpleangularapp -p 80:80 simple-angular-app:latest
 echo "\n"
 echo "==========================Simpleangularapp container is running now=========================="
-
+echo "admin" | sudo -S docker tag simple-angular-app:latest 822642595439.dkr.ecr.ap-south-2.amazonaws.com/simple-angular-app:latest
+echo "admin" | sudo -S docker push 822642595439.dkr.ecr.ap-south-2.amazonaws.com/simple-angular-app:latest
