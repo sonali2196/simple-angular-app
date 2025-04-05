7a0d49b038eff393318d62a3e6f6c71dd50e88b0
Manthan <kafax55297@abaot.com>
2025-04-05 20:20:39 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 echo "admin" | sudo -S docker build -t simple-angular-app .
+echo "Build has been completed"
 echo "admin" | sudo -S docker stop simpleangularapp
+echo "Simpleangularapp container has been stopped"
 echo "admin" | sudo -S docker rm simpleangularapp
+echo "Simpleangularapp container has been removed"
 echo "admin" | sudo -S docker run -d --name simpleangularapp -p 80:80 simple-angular-app:latest
+echo "Simpleangularapp container is running now"
 
