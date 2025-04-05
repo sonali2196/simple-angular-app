f56d0775e1b03fffcc0ef6ab6c9822b5b91c025f
Manthan <kafax55297@abaot.com>
2025-04-05 20:11:20 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-echo "admin" | sudo docker build -t simple-angular-app .
-echo "admin" | sudo docker stop (docker ps -q)
-echo "admin" | sudo docker rm (docker ps -q)
-echo "admin" | sudo docker run -d --name simpleangularapp -p 80:80 simple-angular-app:latest
+echo "admin" | sudo -S docker build -t simple-angular-app .
+
